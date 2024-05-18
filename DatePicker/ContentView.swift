b1b83113a70a54dcbe0e9b0f480ef74a95210db0
Hossein Zare <168444477+realzare@users.noreply.github.com>
2024-05-18 13:16:26 +0330

    ContentView.swift

--- DatePicker/ContentView.swift
+++ DatePicker/ContentView.swift
@@ -8,17 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selectedDate = Date()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        Form {
+            DatePicker("selected a date",selection: $selectedDate)
+                .datePickerStyle(.graphical)
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
