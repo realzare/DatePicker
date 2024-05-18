c6d5aa0a4023e1f03f89eed05d2972903d205047
realzare <realhosseinzare@gmail.com>
2024-05-18 12:28:10 +0330

    Initial Commit

---
+++ DatePicker/DatePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePickerApp.swift
+//  DatePicker
+//
+//  Created by Hossein Zare on 5/18/24.
+//
+
+import SwiftUI
+
+@main
+struct DatePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
